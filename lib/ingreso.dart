85d7fd827d7433138e3b1090f93bbd24b4debefa
DanielGiron20 <girondaniel388@gmail.com>
2024-04-19 18:29:00 -0600

    Primer commit al proyecto

--- lib/ingreso.dart
+++ lib/ingreso.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class AddBandScreen extends StatefulWidget {
   @override
@@ -10,46 +14,72 @@ class _AddBandScreenState extends State<AddBandScreen> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _albumController = TextEditingController();
   final TextEditingController _yearController = TextEditingController();
-final TextEditingController _imageUrlController = TextEditingController(); 
+  File? _imageFile; // Variable para almacenar la imagen seleccionada
+
+  // Función para agregar una banda a Firestore
   Future<void> _addBand() async {
+    // Verifica si se ha seleccionado una imagen
+    if (_imageFile == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Debes seleccionar una imagen')),
+      );
+      return;
+    }
+
+    // Sube la imagen a Firebase Storage y obtén su URL de descarga
+    String? imageUrl = await uploadImage(_imageFile!);
+
     try {
-      // Validaciones
+      // Validamos que los campos no estén vacíos
       if (_nameController.text.isEmpty ||
           _albumController.text.isEmpty ||
-          _yearController.text.isEmpty || 
-          _imageUrlController.text.isEmpty) {
+          _yearController.text.isEmpty ||
+          imageUrl == null) {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Todos los campos son obligatorios')),
         );
         return;
       }
 
-      
+      // Creamos un nuevo documento en la colección 'bandas' con la URL de la imagen
       await FirebaseFirestore.instance.collection('bandas').add({
         'name': _nameController.text,
         'album': _albumController.text,
         'year': _yearController.text,
-        'votes': 0,
-        'imageUrl': _imageUrlController.text, // Inicializamos los votos en 0
+        'votes': 0, // Inicializamos los votos en 0
+        'imageUrl': imageUrl, // Agregamos la URL de la imagen
       });
 
-      
+      // Limpiamos los campos de texto después de agregar la banda exitosamente
       _nameController.clear();
       _albumController.clear();
       _yearController.clear();
-      _imageUrlController.clear();
+      setState(() {
+        _imageFile = null; // Limpiamos el archivo de imagen seleccionado
+      });
 
-  
+      // Mostramos un mensaje de éxito
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Banda agregada exitosamente')),
       );
     } catch (e) {
-      
+      // Manejamos cualquier error que pueda ocurrir durante la operación
       print('Error al agregar banda: $e');
-
+      // Mostramos un mensaje de error
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Error al agregar banda')),
       );
+    }
+  }
+
+  // Función para seleccionar una imagen desde la galería o tomar una foto
+  Future<void> _pickImage(ImageSource source) async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: source);
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = File(pickedFile.path);
+      });
     }
   }
 
@@ -59,42 +89,70 @@ final TextEditingController _imageUrlController = TextEditingController();
       appBar: AppBar(
         title: Text('Agregar Banda'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              controller: _nameController,
-              decoration: InputDecoration(labelText: 'Nombre'),
-            ),
-            TextField(
-              controller: _albumController,
-              decoration: InputDecoration(labelText: 'Álbum'),
-            ),
-            TextField(
-              controller: _yearController,
-              decoration: InputDecoration(labelText: 'Año'),
-              keyboardType: TextInputType.number,
-            ),
-            TextField(
-              controller: _imageUrlController, 
-              decoration: InputDecoration(labelText: 'URL de la imagen'), 
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: _addBand, 
-              child: Text('Agregar Banda'),
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                controller: _nameController,
+                decoration: InputDecoration(labelText: 'Nombre'),
+              ),
+              TextField(
+                controller: _albumController,
+                decoration: InputDecoration(labelText: 'Álbum'),
+              ),
+              TextField(
+                controller: _yearController,
+                decoration: InputDecoration(labelText: 'Año'),
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () => _pickImage(ImageSource.gallery),
+                child: Text('Seleccionar de la galería'),
+              ),
+              SizedBox(height: 8),
+              ElevatedButton(
+                onPressed: () => _pickImage(ImageSource.camera),
+                child: Text('Tomar una foto'),
+              ),
+              SizedBox(height: 16),
+              _imageFile != null
+                  ? Image.file(_imageFile!, height: 200)
+                  : SizedBox(),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: _addBand,
+                child: Text('Agregar Banda'),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-void main() {
-  runApp(MaterialApp(
-    home: AddBandScreen(),
-  ));
+// Función para subir una imagen a Firebase Storage
+Future<String?> uploadImage(File imageFile) async {
+  try {
+    // Obtén una referencia al bucket de Firebase Storage
+    final Reference storageRef =
+        FirebaseStorage.instance.ref().child('band_images/${DateTime.now().millisecondsSinceEpoch}.jpg');
+
+    // Sube la imagen al bucket
+    final UploadTask uploadTask = storageRef.putFile(imageFile);
+
+    // Espera a que se complete la subida y obtén la URL de la imagen
+    final TaskSnapshot snapshot = await uploadTask;
+    final String downloadUrl = await snapshot.ref.getDownloadURL();
+
+    // Devuelve la URL de descarga de la imagen
+    return downloadUrl;
+  } catch (e) {
+    print('Error al subir imagen: $e');
+    return null;
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
-import 'ingreso.dart'; 
+import 'ingreso.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -38,7 +38,7 @@ class Home extends StatelessWidget {
           IconButton(
             icon: Icon(Icons.add),
             onPressed: () {
-              Navigator.pushNamed(context, '/ingreso'); 
+              Navigator.pushNamed(context, '/ingreso'); // Abre la pantalla de ingreso al presionar el botón
             },
           ),
         ],
@@ -56,7 +56,7 @@ class Home extends StatelessWidget {
             return ListView.builder(
               itemCount: snapshot.data!.docs.length,
               itemBuilder: (context, index) {
-                var bandData = snapshot.data!.docs[index].data() as Map<String, dynamic>?; 
+                var bandData = snapshot.data!.docs[index].data() as Map<String, dynamic>?; // Convertir a Map<String, dynamic>
                 return GestureDetector(
                   onTap: () {
                     incrementVote(snapshot.data!.docs[index].id);
@@ -65,12 +65,14 @@ class Home extends StatelessWidget {
                     title: Text(bandData?['name'] ?? ''),
                     subtitle: Text('${bandData?['album'] ?? ''} (${bandData?['year'] ?? ''})'),
                     trailing: Text('Votos: ${bandData?['votes'] ?? ''}'),
-                    leading: Image.network(
-                      bandData?['imageUrl'] ?? '', 
-                      width: 50, 
-                      height: 50,
-                      fit: BoxFit.cover, 
-                    ),
+                    leading: bandData?['imageUrl'] != null
+                        ? Image.network(
+                            bandData?['imageUrl'], // Usar la URL directa almacenada en Firestore
+                            width: 50, // Ajustar el ancho de la imagen según sea necesario
+                            height: 50, // Ajustar la altura de la imagen según sea necesario
+                            fit: BoxFit.cover, // Ajustar la forma en que se ajusta la imagen dentro del widget
+                          )
+                        : Container(), // Si no hay URL de imagen, muestra un contenedor vacío
                   ),
                 );
               },
@@ -91,6 +93,3 @@ void incrementVote(String bandId) async {
     print('Error al registrar el voto para la banda con ID: $bandId, Error: $e');
   }
 }
-
-
-
